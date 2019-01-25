f2b76fd9a419437042844878dc093fef63cb077f
jculver1 <jculver1900@gmail.com>
2019-01-25 09:57:32 -0700

    updated migration and seeds to include additional columns.

--- back-end/migrations/migration.js
+++ back-end/migrations/migration.js
@@ -2,6 +2,9 @@ exports.up = function(knex, Promise) {
     return knex.schema.createTable('methods', table => {
         table.increments('id').unsigned().primary();
         table.string('name').notNullable().defaultsTo('');
+        table.string('description').notNullable().defaultsTo('');
+        table.string('example').notNullable().defaultsTo('');
+        table.string('link').notNullable().defaultsTo('');
       })
 };
 
