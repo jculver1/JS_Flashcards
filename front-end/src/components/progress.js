84f682a50ce18d767972b43baf19c72490a50f8e
jculver1 <jculver1900@gmail.com>
2019-02-01 15:09:33 -0700

    changed css for progress bar from app.css to index.css, see if makes difference?

--- front-end/src/components/progress.js
+++ front-end/src/components/progress.js
@@ -2,12 +2,13 @@ import React from 'react';
 import '../App.css';
 
 
-
 const Progress = (props) => {
 return(
+
 <div class="progress mt-5">
   <div class="progress-bar progress-bar-striped bg-success" role="progressbar"  aria-valuenow= {props.percent} aria-valuemin="0" style={{width: `${props.percent}%`}}aria-valuemax="100"></div>
 </div>
+
 )}
 
 export default Progress
